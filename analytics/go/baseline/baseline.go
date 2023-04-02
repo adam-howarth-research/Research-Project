2394840f304dfd021cd31c90cf7c48adf89187c2
adam howarth <adam.howarth@plixer.com>
2023-04-02 17:20:46 -0400

    baseline init

---
+++ analytics/go/baseline/baseline.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+
+)
+
+func compute(nodes int, factor int) {
+	// Simulate the time complexity by performing operations proportional to nodes * factor
+	operations := nodes * factor
+
+	for i := 0; i < operations; i++ {
+		// Perform a simple operation (e.g., addition) to simulate the computation
+		_ = rand.Float64() + rand.Float64()
+	}
+}
+
+func main() {
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMicro
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	if len(os.Args) != 4 {
+		log.Error().Msg("Usage: ./baseline <node_count> <time_complexity_factor>")
+		os.Exit(1)
+	}
+
+	nodes, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Error().Msg("Error: Invalid node_count")
+		os.Exit(1)
+	}
+
+	factor, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Error().Msg("Error: Invalid time_complexity_factor")
+		os.Exit(1)
+	}
+
+	freq, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		log.Error().Msg("Error: Invalid run frequency")
+		os.Exit(1)
+	}
+
+	ticker := time.NewTicker(time.Duration(freq) * time.Second)
+
+	for {
+		select {
+		case <-ticker.C:
+			start := time.Now()
+			compute(nodes, factor)
+			elapsed := time.Since(start)
+			elapsedMS := elapsed.Microseconds()
+
+			log.Info().Time("start", start).Int("nodes", nodes).Int("factor", factor).Int64("elapsed", elapsedMS).Msgf("Computation with node count %d and time complexity factor %d took %s\n", nodes, factor, elapsed)
+
+		
+			log.Debug().Msgf("Computation with node count %d and time complexity factor %d took %s\n", nodes, factor, elapsed)
+			}
+	}
+
+}
